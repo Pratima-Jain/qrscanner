ac7658cb05eec0754f3e4b4be0ebffcef58d4c0d
Pratima Jain <pratimajain1115@gmail.com>
2025-08-01 22:33:20 +0530

    Update qrscanner.py

--- qrscanner.py
+++ qrscanner.py
@@ -1,5 +1,5 @@
 import qrcode  # type: ignore
 
-myqr= qrcode.make("https://github.com/Pratima-Jain?tab=repositories")
+myqr= qrcode.make("https://github.com/Pratima-Jain")
 myqr.save("Pratima Jain_GitHub.png")
 
